a763af46e795666cb4881d02cca2e54bdf7cc3fd
mprotic123 <milan.protic@ts9maj.edu.rs>
2022-01-21 10:08:41 +0100

    Fixed ESLint errors

--- src/functionalities.test.js
+++ src/functionalities.test.js
@@ -60,27 +60,27 @@ describe('should edit Item', () => {
     expect(result).toBe('Hello world');
   });
 
-test('should Edit Item in the browser', () => {
-  const inputValue = EditDom.window.document.getElementById('edit').value;
-  const index = 0;
+  test('should Edit Item in the browser', () => {
+    const inputValue = EditDom.window.document.getElementById('edit').value;
+    const index = 0;
+    const storage = new MockStorage();
+    const data = [];
+    data.push(task);
+    storage.setItem('list', data);
+    const arr = storage.getItem('list');
+    expect(Tasks.editeTextAreaDom(arr, index, inputValue)).toBe('hello');
+  });
+});
+
+test('should update complete status in local storage', () => {
+  const checkbox = UpdateDom.window.document.getElementById('0');
   const storage = new MockStorage();
   const data = [];
   data.push(task);
   storage.setItem('list', data);
-  const arr = storage.getItem('list');
-  expect(Tasks.editeTextAreaDom(arr, index, inputValue)).toBe('hello');
-  });
-});
-
-test('should update complete status in local storage', () => {  
-  const checkbox = UpdateDom.window.document.getElementById('0');  
-  const storage = new MockStorage();  
-  const data = [];  
-  data.push(task);  
-  storage.setItem('list', data);  
-  const id = 0;  checkbox.click((e) => {    
-    Tasks.completeStatus(id, e);    
-    expect(Tasks.localData()[id].complete).toBeTruthy();  
+  const id = 0; checkbox.click((e) => {
+    Tasks.completeStatus(id, e);
+    expect(Tasks.localData()[id].complete).toBeTruthy();
   });
 });
 test('should update complete status in local storage', () => {
@@ -148,11 +148,3 @@ describe('should edit Item', () => {
     expect(Tasks.editeTextAreaDom(arr, index, inputValue)).toBe('hello');
   });
 });
-
-
-
-
-
-
-
-
